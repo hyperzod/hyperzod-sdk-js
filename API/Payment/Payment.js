f29d4641ba334b03c229d62fd1f0553b17f5cb54
THEC0Der7 <aa4223@live.mdx.ac.uk>
2021-12-16 19:54:37 +0500

    stripe intant end point added

--- API/Payment/Payment.js
+++ API/Payment/Payment.js
@@ -6,4 +6,10 @@ module.exports = {
         const url = `/payment/pg/callback/${method}`
         return Request.get(`${url}`, { params: { ...payload } })
     },
+
+    //Stripe Payment Intent 
+    stripeIntent(payload) {
+        const url = `/payment/pg/paymentIntent/stripe`
+        return Request.post(`${url}`,  payload)
+    },
 }
