a61f653a318c2adb0dcf19d37461820425d1ed19
khwaleed2 <93437825+THEC0Der7@users.noreply.github.com>
2022-02-22 18:32:40 +0500

    payment callback api upadated

--- API/Payment/Payment.js
+++ API/Payment/Payment.js
@@ -3,7 +3,7 @@ const Request = require('../../Request');
 module.exports = {
     //Create Paymemt 
     createPayment(method, payload) {
-        const url = `/store/v1/payment/pgcallback/order/${method}`
+        const url = `/store/v1/payment/pg/callback/${method}`
         return Request.get(`${url}`, { params: { ...payload } })
     },
 
