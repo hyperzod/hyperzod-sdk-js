2acfbc82f04ff68303741c5edfd5bbb9141be9fd
Salman Arshad <sal@hyperzod.com>
2022-04-29 01:04:32 +0530

    update push auth url

--- API/Notification/Notification.js
+++ API/Notification/Notification.js
@@ -3,7 +3,7 @@ const Request = require('../../Request');
 module.exports = {
     //Push Auth 
     pushAuth(payload) {
-        const url = `/store/v1/notification/push/auth`
+        const url = `/store/v1/notification/push/auth/customer`
         return Request.post(`${url}`, payload)
     },
 }
