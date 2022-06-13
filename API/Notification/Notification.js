129d67671c78d57650d94a41a4c5a69ae4b33b47
Salman Arshad <sal@hyperzod.com>
2022-06-13 07:48:09 +0530

    change push token route

--- API/Notification/Notification.js
+++ API/Notification/Notification.js
@@ -3,7 +3,7 @@ const Request = require('../../Request');
 module.exports = {
     //Push Auth 
     pushAuth(payload) {
-        const url = `/store/v1/notification/push/auth/customer`
+        const url = `public/v1/notification/push/token`
         return Request.post(`${url}`, payload)
     },
 }
