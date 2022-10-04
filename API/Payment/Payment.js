b8e000a67bba57be10ea448255f07b43ce34ce1c
Gulzar Ahmed <alignwebs@gmail.com>
2022-10-04 16:34:07 +0530

    createPaymentIntent

--- API/Payment/Payment.js
+++ API/Payment/Payment.js
@@ -1,15 +1,15 @@
 const Request = require('../../Request');
 
 module.exports = {
-    //Create Paymemt 
+    // Create Paymemt 
     createPayment(method, payload) {
         const url = `/store/v1/payment/pg/callback/${method}`
         return Request.get(`${url}`, { params: { ...payload } })
     },
 
-    //Stripe Payment Intent 
-    stripeIntent(payload) {
-        const url = `/store/v1/payment/pg/paymentIntent/stripe`
-        return Request.post(`${url}`,  payload)
+    // Payment Intent 
+    createPaymentIntent(pg_mode, payload) {
+        const url = `/store/v1/payment/pg/paymentIntent/${pg_mode}`
+        return Request.post(`${url}`, payload)
     },
 }
