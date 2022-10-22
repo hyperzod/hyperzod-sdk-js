fa229fcf7bb69684f27f5f3d20598bdcc165aacf
khwaleed96 <khwaleed56@gmail.com>
2022-10-22 16:36:19 +0500

    payment modes end point added

--- API/Payment/Payment.js
+++ API/Payment/Payment.js
@@ -1,6 +1,12 @@
 const Request = require('../../Request');
 
 module.exports = {
+    // Get Payment Modes
+    getPaymentModes(payload) {
+        const url = `/store/v1/payment/payment-mode/tenant/client`
+        return Request.get(`${url}`, { params: { ...payload } })
+    },
+
     // Create Paymemt 
     createPayment(method, payload) {
         const url = `/store/v1/payment/pg/callback/${method}`
