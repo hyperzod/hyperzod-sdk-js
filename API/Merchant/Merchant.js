9c97083d89094aede3d9759d830c8a96bf119985
khwaleed96 <khwaleed56@gmail.com>
2023-04-07 12:20:37 +0500

    Nearest Merchant end point added

--- API/Merchant/Merchant.js
+++ API/Merchant/Merchant.js
@@ -1,34 +1,40 @@
-const Request = require('../../Request');
+const Request = require("../../Request");
 
 module.exports = {
-    // Find Nearby Merchants
-    findNearByMerchants(payload) {
-        const url = `/store/v1/merchant/nearby`
-        return Request.post(`${url}`, payload)
-    },
+  // Find Nearby Merchants
+  findNearByMerchants(payload) {
+    const url = `/store/v1/merchant/nearby`;
+    return Request.post(`${url}`, payload);
+  },
 
-    // List Featured Merchants
-    listFeaturedMerchants(payload) {
-        const url = `/store/v1/merchant/merchant-featured/list`
-        return Request.get(`${url}`, { params: { ...payload }})
-    },
+  // List Featured Merchants
+  listFeaturedMerchants(payload) {
+    const url = `/store/v1/merchant/merchant-featured/list`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
 
-    // List Sponsored Merchants
-    listSponsoredMerchants(payload) {
-        const url = `/store/v1/merchant/merchant-sponsored/list`
-        return Request.get(`${url}`, { params: { ...payload }})
-    },
+  // List Sponsored Merchants
+  listSponsoredMerchants(payload) {
+    const url = `/store/v1/merchant/merchant-sponsored/list`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
 
-    // Get Merchant Menu
-    getMerchantMenu(payload) {
-        const url = `/store/v1/merchant/menu`
-        return Request.get(`${url}`, { params: { ...payload }})
-    },
+  // Get Merchant Menu
+  getMerchantMenu(payload) {
+    const url = `/store/v1/merchant/menu`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
 
-    // List Merchant Categories
-    listMerchantCategories(payload) {
-        payload.status = true;
-        const url = `/store/v1/merchant/categories`
-        return Request.get(`${url}`, { params: { ...payload }})
-    },
-}
+  // Get Nearest Merchant
+  nearestMerchant(payload) {
+    const url = `/store/v1/merchant/nearest`;
+    return Request.post(`${url}`, payload);
+  },
+
+  // List Merchant Categories
+  listMerchantCategories(payload) {
+    payload.status = true;
+    const url = `/store/v1/merchant/categories`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
+};
