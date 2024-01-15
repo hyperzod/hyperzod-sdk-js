7b175b7a955abafd815835210c11bf1cf86c9dfb
khwaleed96 <khwaleed56@gmail.com>
2024-01-15 16:39:22 +0500

    public file upload endpoint added

--- API/Upload/Upload.js
+++ API/Upload/Upload.js
@@ -2,9 +2,11 @@ const Request = require('../../Request');
 
 module.exports = {
     // upload files
-    uploadFiles(payload) {
-        const api_url_dev = 'https://upload.hyperzod.dev/upload';
-        const api_url_production = 'https://upload.hyperzod.app/upload';
+    uploadFiles(payload, type) {
+        const uploadType = type === 'public' ? 'public-upload' : 'upload';
+
+        const api_url_dev = `https://upload.hyperzod.dev/${uploadType}`;
+        const api_url_production = `https://upload.hyperzod.app/${uploadType}`;
 
         const url = window.HYPERZOD_API_ENV == 'production' ? api_url_production : api_url_dev
         return Request.post(`${url}`, payload, {
