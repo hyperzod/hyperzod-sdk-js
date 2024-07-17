6441ae4822ec1b94e494bf8b17df5b39a0c4128c
Waleed Ahmad <kkhwaleed56@gmail.com>
2024-07-17 14:58:13 +0500

    get carts list api added

--- API/Cart/Cart.js
+++ API/Cart/Cart.js
@@ -1,45 +1,52 @@
-const Request = require('../../Request');
+const Request = require("../../Request");
 
 module.exports = {
-    // Update Cart
-    updateCart(payload) {
-        const url = `/store/v1/cart`
-        return Request.post(`${url}`, payload)
-    },
+  // Update Cart
+  updateCart(payload) {
+    const url = `/store/v1/cart`;
+    return Request.post(`${url}`, payload);
+  },
 
-    // Delete Cart
-    deleteCart(payload) {
-        const url = `/store/v1/cart`
-        return Request.delete(`${url}`, { params: { ...payload }})
-    },
+  // Delete Cart
+  deleteCart(payload) {
+    const url = `/store/v1/cart`;
+    return Request.delete(`${url}`, { params: { ...payload } });
+  },
 
-    // Apply Coupon
-    applyCoupon(payload) {
-        const url = `/store/v1/cart/coupon/apply`
-        return Request.post(`${url}`, payload)
-    },
+  // Apply Coupon
+  applyCoupon(payload) {
+    const url = `/store/v1/cart/coupon/apply`;
+    return Request.post(`${url}`, payload);
+  },
 
-    // Remove Coupon
-    removeCoupon(payload) {
-        const url = `/store/v1/cart/coupon/remove`
-        return Request.post(`${url}`, payload)
-    },
+  // Remove Coupon
+  removeCoupon(payload) {
+    const url = `/store/v1/cart/coupon/remove`;
+    return Request.post(`${url}`, payload);
+  },
 
-    // Fetch Cart
-    getCart(payload) {
-        const url = `/store/v1/cart`
-        return Request.get(`${url}`, { params: { ...payload }})
-    },
+  // Fetch Cart
+  getCart(payload) {
+    const url = `/store/v1/cart`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
 
-    // Validate Cart
-    validateCart(payload) {
-        const url = `/store/v1/cart/validate`
-        return Request.get(`${url}`, { params: { ...payload }})
-    },
+  // Fetch Carts
+  getCarts(payload) {
+    console.log(payload);
+    const url = `/store/v1/cart/list-by-ids`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
 
-    // Validate Product
-    validateProduct(payload) {
-        const url = `/store/v1/cart/validate/product`
-        return Request.post(`${url}`, payload)
-    },
-}
+  // Validate Cart
+  validateCart(payload) {
+    const url = `/store/v1/cart/validate`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
+
+  // Validate Product
+  validateProduct(payload) {
+    const url = `/store/v1/cart/validate/product`;
+    return Request.post(`${url}`, payload);
+  },
+};
