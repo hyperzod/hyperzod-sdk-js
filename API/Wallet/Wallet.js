f52196f51062e00af941cce90b3efb7223ee2364
khwaleed96 <khwaleed56@gmail.com>
2025-03-20 12:28:11 +0500

    wallet apis added

--- API/Wallet/Wallet.js
+++ API/Wallet/Wallet.js
@@ -1,15 +1,33 @@
-const Request = require('../../Request');
+const Request = require("../../Request");
 
 module.exports = {
-    // Get Wallet Transaction History
-    getWalletTransactionHistory(payload) {
-        const url = `/wallet/transaction-history`
-        return Request.get(`${url}`, { params: { ...payload }})
-    },
+  // Get Wallet Transaction History
+  getWalletTransactionHistory(payload) {
+    const url = `/wallet/transaction-history`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
 
-    // Get Wallet Info
-    getWalletInfo(payload) {
-        const url = `/wallet/wallet-info`
-        return Request.get(`${url}`, { params: { ...payload }})
-    },
-}
+  // Get Wallet Info
+  getWalletInfo(payload) {
+    const url = `/wallet/wallet-info`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
+
+  // Get User Wallet
+  getWallet(payload) {
+    const url = `/store/v1/wallet`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
+
+  // Apply Wallet
+  applyWallet(payload) {
+    const url = `/store/v1/cart/wallet/apply`;
+    return Request.post(`${url}`, payload);
+  },
+
+  // Remove Wallet
+  removeWallet(payload) {
+    const url = `/store/v1/cart/wallet/remove`;
+    return Request.post(`${url}`, payload);
+  },
+};
