ec49223c5aaf1afe10bdee0f1193251c56bafb86
Firas Mumtaz <firas@hyperzod.com>
2025-04-08 12:58:50 +0530

    change route name

--- API/Stats/Stats.js
+++ API/Stats/Stats.js
@@ -4,7 +4,7 @@ const Request = require('../../Request');
 module.exports = {
     // Check User is in Segment
     checkUserInSegment: function(payload) {
-        const url = `/store/v1/stats/checkUser`
+        const url = `/store/v1/stats/checkUserInSegment`
         return Request.get(`${url}`, { params: { ...payload } });
     }
 }
