9457c4067cda2590c86ff7d98c5c1b314d4a936d
Firas Mumtaz <firas@hyperzod.com>
2025-04-19 15:17:43 +0530

    update version to 1.0.81 and refactor search API methods for consistency

--- API/Search/Search.js
+++ API/Search/Search.js
@@ -8,28 +8,36 @@ module.exports = {
         return Request.get(`${url}`, { params: { ...payload }});
     },
 
+    
     // Product by category
     getProductsByCategory(payload) {
-        const url = `/store/v1/search/product-category/products`;
-        return Request.get(`${url}`, { params: { ...payload } });
+      const url = `/store/v1/search/product-category/products`;
+      return Request.get(`${url}`, { params: { ...payload } });
     },
   
     // Paginated products categories
     getProductCategories(payload) {
-        const url = `/store/v1/search/product-category/list`;
-        return Request.get(`${url}`, { params: { ...payload } });
+      const url = `/store/v1/search/product-category/list`;
+      return Request.get(`${url}`, { params: { ...payload } });
     },
-
-    getProductCategoriesByIds(payload) {
-        const url = `/store/v1/search/product-category/list-by-ids`;
-        return Request.get(`${url}`, { params: { ...payload } });
-    },
-
-    getProduct(payload) {
-        const url = `/store/v1/search/product/view`;
-        return Request.get(`${url}`, { params: { ...payload } });
-      },
     
+    getProductCategoriesByIds(payload) {
+      const url = `/store/v1/search/product-category/list-by-ids`;
+      return Request.get(`${url}`, { params: { ...payload } });
+    },
+    
+    getProduct(payload) {
+      const url = `/store/v1/search/product/view`;
+      return Request.get(`${url}`, { params: { ...payload } });
+    },
+    
+    //Get Product by Ids
+    getProductByIds(payload) {
+      const url = `/store/v1/search/product/list-by-ids`;
+      return Request.get(`${url}`, { params: { ...payload } });
+    },
+    
+    //Search Merchant Products
     searchMerchantProducts(payload) {
       const url = `/store/v1/search/merchant/products`;
       return Request.get(`${url}`, { params: { ...payload } });
