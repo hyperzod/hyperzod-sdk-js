e11f0fd61da19fbaf4f2f1d44a187a97e7e628e3
Firas Mumtaz <firas@hyperzod.com>
2025-08-13 18:46:15 +0530

    update version to 1.0.85 and add recommendSimilarItems method to Recommendation service

--- API/Recommendation/Recommendation.js
+++ API/Recommendation/Recommendation.js
@@ -1,20 +1,23 @@
-const Request = require('../../Request');
+const Request = require("../../Request");
 
 module.exports = {
-
   recommendProductsWithinMerchant(payload) {
-    const url = `/store/v1/recommend/products/merchant`
-    return Request.post(`${url}`, payload)
-},
+    const url = `/store/v1/recommend/products/merchant`;
+    return Request.post(`${url}`, payload);
+  },
 
   recommendMerchantsForUser(payload) {
-    const url = `/store/v1/recommend/merchants`
-    return Request.post(`${url}`, payload.data, {headers: payload.headers})
-},
+    const url = `/store/v1/recommend/merchants`;
+    return Request.post(`${url}`, payload.data, { headers: payload.headers });
+  },
 
   recommendProductsForUser(payload) {
-    const url = `/store/v1/recommend/products`
-    return Request.post(`${url}`, payload)
-},
+    const url = `/store/v1/recommend/products`;
+    return Request.post(`${url}`, payload);
+  },
 
+  recommendSimilarItems(payload) {
+    const url = `/store/v1/recommend/similarItems`;
+    return Request.post(`${url}`, payload);
+  },
 };
