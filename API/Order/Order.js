957cc0bf065d7ef51f5ff35cdd03867595156a05
Firas Mumtaz <firas@hyperzod.com>
2025-08-19 14:50:44 +0530

    update version to 1.0.86 and added getOrderTip  API

--- API/Order/Order.js
+++ API/Order/Order.js
@@ -1,28 +1,33 @@
-const Request = require('../../Request');
+const Request = require("../../Request");
 
 module.exports = {
-    //Place Order 
-    placeOrder(payload) {
-        const url = `/store/v1/order`
-        return Request.post(`${url}`, payload)
-    },
+  //Place Order
+  placeOrder(payload) {
+    const url = `/store/v1/order`;
+    return Request.post(`${url}`, payload);
+  },
 
-    //fetch Orders 
-    getOrders(payload) {
-        const url = `/store/v1/order/getOrdersByUserId`
-        return Request.get(`${url}`, { params: { ...payload }})
-    },
+  //fetch Orders
+  getOrders(payload) {
+    const url = `/store/v1/order/getOrdersByUserId`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
 
-    //fetch Orders
-    getOrder(payload) {
-        const url = `/store/v1/order`
-        return Request.get(`${url}`, { params: { ...payload }})
-    },
+  //fetch Orders
+  getOrder(payload) {
+    const url = `/store/v1/order`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
 
-    //fetch order schedulling slots
-    getOrderSchedulling(payload) {
-        const url = `/store/v1/order/getSchedulingSlots`
-        return Request.get(`${url}`, { params: { ...payload }})
-    },
+  //fetch order schedulling slots
+  getOrderSchedulling(payload) {
+    const url = `/store/v1/order/getSchedulingSlots`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
 
-}
+  //Get Order Tips
+  getOrderTip(payload) {
+    const url = `/store/v1/order/getOrderTip`;
+    return Request.get(`${url}`, { params: { ...payload } });
+  },
+};
