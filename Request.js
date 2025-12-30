9aebcfc17fd7aa8a764f7bbf03c14808ff499cbb
khwaleed96 <khwaleed56@gmail.com>
2025-12-30 15:55:17 +0500

    api base URL new configuration

--- Request.js
+++ Request.js
@@ -1,19 +1,28 @@
-const axios = require('axios')
+const axios = require("axios");
 
 // Get Base URL
-const api_url_dev = 'https://api.hyperzod.dev';
-const api_url_production = 'https://api.hyperzod.app';
+const api_url_dev = "https://api.hyperzod.dev";
+const api_url_production = "https://api.hyperzod.app";
 
 const API = axios.create({
-  headers: { 'Content-Type': 'application/json' },
+  headers: { "Content-Type": "application/json" },
 });
 
-API.interceptors.request.use(async config => {
-  config.baseURL = (window.HYPERZOD_API_ENV == 'production' ? api_url_production : api_url_dev);
-  return config;
-}, error => Promise.reject(error));
+function getBaseURL() {
+  const baseUrl = window.HYPERZOD_API_CONFIG.baseURL;
+  return baseUrl
+    ? baseUrl
+    : window.HYPERZOD_API_ENV == "production"
+    ? api_url_production
+    : api_url_dev;
+}
 
+API.interceptors.request.use(
+  async (config) => {
+    config.baseURL = getBaseURL();
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
-module.exports = API
-
-
+module.exports = API;
