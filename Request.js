4c3375006dbd71e3ec17f12dc284b228087b2652
khwaleed96 <khwaleed56@gmail.com>
2026-01-01 11:42:27 +0500

    Revert "api base url logic updated"
    This reverts commit 0653e555b3fd9a1372deb7a5dde4de37bd10a1df.

--- Request.js
+++ Request.js
@@ -1,25 +1,20 @@
 const axios = require("axios");
 
 // Get Base URL
-const baseURLS = {
-  default: {
-    dev: "https://api.hyperzod.dev",
-    production: "https://api.hyperzod.app",
-  },
-  secondary: {
-    dev: "https://api-dev.hyperzod53.com",
-    production: "https://api.hyperzod53.com",
-  },
-};
+const api_url_dev = "https://api.hyperzod.dev";
+const api_url_production = "https://api.hyperzod.app";
 
 const API = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
 function getBaseURL() {
-  const apiType =
-    window.shouldUseSecondaryApi == true ? "secondary" : "default";
-  return baseURLS[apiType][window.HYPERZOD_API_ENV];
+  const baseUrl = window.HYPERZOD_API_CONFIG.baseURL;
+  return baseUrl
+    ? baseUrl
+    : window.HYPERZOD_API_ENV == "production"
+    ? api_url_production
+    : api_url_dev;
 }
 
 API.interceptors.request.use(
