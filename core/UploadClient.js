1f6fe33d363dd58b2d93c17ad92382841d73fbcb
khwaleed96 <khwaleed56@gmail.com>
2026-01-17 11:23:43 +0500

    upload route53 base url fixed

--- core/UploadClient.js
+++ core/UploadClient.js
@@ -7,8 +7,8 @@ const UPLOAD_BASE_URLS = {
     production: "https://upload.hyperzod.app",
   },
   secondary: {
-    dev: "http://upload.hyperzod53.com",
-    production: "http://upload-dev.hyperzod53.com",
+    dev: "https://upload.hyperzod53.com",
+    production: "https://upload-dev.hyperzod53.com",
   },
 };
 
