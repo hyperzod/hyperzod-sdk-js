67968a04c6907a4f68db41055d46aaa0db320744
mohommadhamza22 <mohommadhamza22@gmail.com>
2026-05-19 11:59:41 +0530

    Update version to 2.0.4 and simplify Membership API to retrieve current user membership

--- API/V1/Membership/Membership.js
+++ API/V1/Membership/Membership.js
@@ -1,12 +1,9 @@
 export default function MembershipApi(http) {
   const base = "/store/v1";
   return {
-    // Get Membership by ID
-    getMembership(params = {}) {
-      const { id, ...rest } = params;
-      return http.get(`${base}/membership/user-membership`, {
-        params: { ...(id ? { id } : {}), ...rest },
-      });
+    // Get current user membership
+    getMembership() {
+      return http.get(`${base}/membership/user-membership`);
     },
   };
 }
